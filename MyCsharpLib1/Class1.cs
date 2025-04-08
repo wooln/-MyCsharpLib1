53835c96ce01898b4233ad8ee160737c3aba4f33
徐云金YunjinXu <xuyunjin@foxmail.com>
2025-04-08 14:45:26 +0800

    feat(MyCsharpLib1): 添加 Add 和 Subtract 方法的 XML 注释
    - 为 Add 方法添加 XML 注释，描述加法功能- 为 Subtract 方法添加 XML 注释，描述减法功能
    - 优化代码结构，提高代码可读性和维护性

--- MyCsharpLib1/Class1.cs
+++ MyCsharpLib1/Class1.cs
@@ -2,10 +2,23 @@
 
 public class Class1
 {
+    /// <summary>
+    /// add
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
     public static int Add(int a, int b)
     {
         return a + b;
     }
+    
+    /// <summary>
+    /// subtract
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
     public static int Subtract(int a, int b)
     {
         return a - b;
